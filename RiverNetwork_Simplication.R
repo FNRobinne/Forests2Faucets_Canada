65193ea35249ff7e63cb5f097e61b601d1777866
FNRobinne <francois.robinne@nrcan-rncan.gc.ca>
2022-01-07 11:16:56 -0700

    Name changed for HydroRiver_Simplication script. Former script (cleaning) deleted. Added script for snapping catchment centroid to river network.

--- RiverNetwork_Simplication.R
+++ RiverNetwork_Simplication.R
@@ -1,7 +1,8 @@
 ## ---------------------------
-## Script name: 
+## Script name: RiverNetwork_Simplication.R
 ##
-## Purpose of script:
+## Purpose of script: Simplifies the HydroRiver network so it only keeps streams of Strahler order >2,
+##                    except in catchments where only order 1 exist—those are kept.
 ##
 ## Author: Dr. François-Nicolas Robinne
 ##
@@ -25,14 +26,14 @@ setwd("D:/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE")
 options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
 memory.limit(30000000)     # this is needed on some PCs to increase memory allowance
 
-## load libraries ---------------------------
+## load libraries --------------------------------------------------------
 
 library(tidyverse)
 library(sf)
 
 # source("functions/packages.R")       # loads up all the packages we need
 
-## load functions ---------------------------
+## load functions --------------------------------------------------------
 
 # source("functions/summarise_data.R") 
 
@@ -43,9 +44,10 @@ library(sf)
   HYBAS_Fraser <- st_read("02_PROCESSED_DATA/HydroBasin_HUC12_Fraser_Fixed.shp") %>%
     st_transform(Fraser, crs = 3979)
   HYRIV_Fraser <- st_read("02_PROCESSED_DATA/HydroRiver_Fraser_Fixed.shp") %>%
-    st_transform(Fraser, crs = 3979)
+    st_transform(Fraser, crs = 3979) %>%
+    select(HYRIV_ID, ORD_STRA, HYBAS_L12)
 
-# Data preparation --------------------------------------------------------
+# Data processing ---------------------------------------------------------
   
   # Separate watersheds that only have Strahler order 1
   # Create river layer for watersheds with more than Strahler order 1
@@ -67,9 +69,15 @@ library(sf)
   HYRIV_Lite <- HYRIV_Fraser %>%
     filter(ORD_STRA > 1) %>% 
     bind_rows(HYRIV_Stral1)
+   
+    # ESDA (just checking that process worked)
+    ggplot() +
+      geom_sf(data = HYRIV_Lite, colour = "blue") +
+      geom_sf(data = HYBAS_Fraser, fill = NA)
     
-  
-  # ESDA
-  ggplot() +
-    geom_sf(data = HYRIV_Lite, colour = "blue") +
-    geom_sf(data = HYBAS_Fraser, fill = NA)
+# Data export -------------------------------------------------------------
+
+  # Save clean river network
+  # Modify destination path as needed
+  st_write(HYRIV_Lite, "02_PROCESSED_DATA/HydroRiver_Fraser_Lite.gpkg", append = F)
+    
---
+++ S2T_SnapCentroids.R
@@ -0,0 +1,48 @@
+library(sf)
+library(dplyr)
+library(rgeos)
+library(maptools) # warning that this package will be retired by the end of 2023
+                  # By then, I hope the snap function from sf works better
+
+setwd("C:/Users/frobinne/Documents/Professionel/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/03_ANALYSIS/CODE/RIVERDIST")
+
+pts <- st_read("TEST_DATA/Subset_BA_Centroids_RiverdistDemo.shp") %>%
+  st_transform(crs = 3979)
+rivs <- st_read("TEST_DATA/Subset_HR_RiverdistDemo.shp") %>%
+  st_transform(crs = 3979)
+ws <- st_read("TEST_DATA/Subset_BA_RiverdistDemo.shp") %>%
+  st_transform(crs = 3979)
+
+# Filter to remove rivers of order one
+
+rivs_two <- rivs %>%
+  filter(ORD_STRA > 1) %>% # will be updated with more advanced filter
+  group_by(HYBAS_L12) %>% 
+  summarise() %>%
+  st_cast
+
+# Loop
+
+# Creates empty SF to collect results from snap
+snap_pts <- pts %>%
+  filter(HYBAS_ID == 1) # Dummy filter to get empty structure
+
+for (pt in pts$HYBAS_ID) {
+  for (riv in rivs_two$HYBAS_L12) {
+    if (pt == riv) {
+      pt_vec <- pts %>%
+        filter(HYBAS_ID == pt)
+      riv_vec <- rivs_two %>%
+        filter(HYBAS_L12 == riv)
+      snappy <- snapPointsToLines(as_Spatial(pt_vec), as_Spatial(riv_vec))
+      snap_pts <- bind_rows(snap_pts, st_as_sf(snappy))
+    }
+  }
+}
+
+snap_pts_spat_join <- snap_pts %>%
+  select(geometry) %>%
+  st_join(ws)
+  
+# Export to geopackage, as sf makes writing shapefile a pain
+st_write(snap_pts_spat_join, "TEST_DATA/Subset_BA_Centroids_SnapTestR.gpkg", append = F)

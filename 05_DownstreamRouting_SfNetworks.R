0ab78fac36cab00dad905dbd4e7805e5381cb5fa
FNRobinne <francois.robinne@nrcan-rncan.gc.ca>
2023-04-28 08:56:40 -0600

    Downstream routing all added variables

--- 05_DownstreamRouting_SfNetworks.R
+++ 05_DownstreamRouting_SfNetworks.R
@@ -50,16 +50,17 @@ for(i in dirs) {
   
   ## Data processing -----------------------------------------------------------
   
+  # Reformat HUC12 outlets by adding HydroBASINS attributes (from previous scripts)
+  dir_outlets <- st_join(outlets, hybas)%>%
+    st_cast(to = "POINT") %>%
+    select(HYBAS_ID.x, NEXT_DOWN, fi_kw_syr, fp_ul_syr, re_th_syr,
+           re_th_s26, re_th_s45, re_th_s85) %>%
+    rename(from = HYBAS_ID.x, to = NEXT_DOWN) %>%
+    mutate(name = as.character(from))
+  
   ##########################
   # Fire intensity routing #
   ##########################
-  
-  # Reformat HUC12 outlets
-  dir_outlets <- st_join(outlets, hybas)%>%
-    st_cast(to = "POINT") %>%
-    select(HYBAS_ID.x, NEXT_DOWN, fi_kw_syr) %>%
-    rename(from = HYBAS_ID.x, to = NEXT_DOWN) %>%
-    mutate(name = as.character(from))
   
   hybas_FI <- hybas %>%
     select(fi_kw_syr)
@@ -95,11 +96,208 @@ for(i in dirs) {
     mutate(HYBAS_ID = as.numeric(HYBAS_ID)) %>%
     select(HYBAS_ID, fi_kw_uyr)
   
-  # Create spatial layers
-  hybas_routed_FI <- inner_join(hybas, routed_FI)
+  ############################
+  # Burn probability routing #
+  ############################
   
-  ## Save outputs -------------------------------------------------------------
-  st_write(hybas_routed_FI, "huc12_Valid_FI.gpkg", delete_layer = T)
+  hybas_BP <- hybas %>%
+    select(fp_ul_syr)
+  
+  # Reformat HydroRIVERS
+  hyriv_line <- st_cast(hyriv, to = "LINESTRING") %>%
+    st_join(hybas_BP, join = st_intersects) # Add fire intensity info
+  
+  # Create a network based on HydroRIVERS
+  net <- as_sfnetwork(hyriv_line, directed = T)
+  # Update river network with outlets
+  new_net <- net %>% # Original river network
+    activate("nodes") %>%
+    st_network_blend(dir_outlets, tolerance = Inf) %>% # Blend in catchment outlets
+    convert(to_spatial_subdivision) %>%
+    activate("edges") %>%
+    mutate(weight = fp_ul_syr) # Use fire intensity as the weight for each river reach
+  # Create origin-destination (OD) matrix (distance-based, here)
+  POI_outlets <- st_as_sf(new_net, "nodes") %>%
+    filter(is.na(name) == F) # Recreate the outlet layer based on their new position after blending (POIs)
+  od_mat <- st_network_cost(new_net, 
+                            from = POI_outlets, to = POI_outlets, # from-to point sf
+                            direction = "in", # inbound
+                            Inf_as_NaN = T)
+  
+  od_mat_tibble <- tibble::as_tibble(od_mat, rownames = "id", .name_repair = "minimal") # converts matrix to tibble and add HYBAS_ID as column
+  od_mat_tibble <- rename(od_mat_tibble, HYBAS_ID = id) # Rename id field to HYBAS_ID for join
+  
+  # Total downstream fire intensity accumulation
+  # Row-wise aggregation (upstream looking downstream, i.e., accumulation)
+  routed_BP <- od_mat_tibble %>%
+    mutate(fp_ul_uyr = rowSums(pick(where(is.numeric), -HYBAS_ID), na.rm = T)) %>%
+    mutate(HYBAS_ID = as.numeric(HYBAS_ID)) %>%
+    select(HYBAS_ID, fp_ul_uyr)
+  
+  #####################
+  # Erosivity routing #
+  #####################
+  
+  hybas_RE <- hybas %>%
+    select(re_th_syr)
+  
+  # Reformat HydroRIVERS
+  hyriv_line <- st_cast(hyriv, to = "LINESTRING") %>%
+    st_join(hybas_RE, join = st_intersects) # Add fire intensity info
+  
+  # Create a network based on HydroRIVERS
+  net <- as_sfnetwork(hyriv_line, directed = T)
+  # Update river network with outlets
+  new_net <- net %>% # Original river network
+    activate("nodes") %>%
+    st_network_blend(dir_outlets, tolerance = Inf) %>% # Blend in catchment outlets
+    convert(to_spatial_subdivision) %>%
+    activate("edges") %>%
+    mutate(weight = re_th_syr) # Use fire intensity as the weight for each river reach
+  # Create origin-destination (OD) matrix (distance-based, here)
+  POI_outlets <- st_as_sf(new_net, "nodes") %>%
+    filter(is.na(name) == F) # Recreate the outlet layer based on their new position after blending (POIs)
+  od_mat <- st_network_cost(new_net, 
+                            from = POI_outlets, to = POI_outlets, # from-to point sf
+                            direction = "in", # inbound
+                            Inf_as_NaN = T)
+  
+  od_mat_tibble <- tibble::as_tibble(od_mat, rownames = "id", .name_repair = "minimal") # converts matrix to tibble and add HYBAS_ID as column
+  od_mat_tibble <- rename(od_mat_tibble, HYBAS_ID = id) # Rename id field to HYBAS_ID for join
+  
+  # Total downstream fire intensity accumulation
+  # Row-wise aggregation (upstream looking downstream, i.e., accumulation)
+  routed_RE <- od_mat_tibble %>%
+    mutate(re_th_uyr = rowSums(pick(where(is.numeric), -HYBAS_ID), na.rm = T)) %>%
+    mutate(HYBAS_ID = as.numeric(HYBAS_ID)) %>%
+    select(HYBAS_ID, re_th_uyr)
+  
+  ###########################
+  # Erosivity RCP26 routing #
+  ###########################
+  
+  hybas_RE26 <- hybas %>%
+    select(re_th_s26)
+  
+  # Reformat HydroRIVERS
+  hyriv_line <- st_cast(hyriv, to = "LINESTRING") %>%
+    st_join(hybas_RE26, join = st_intersects) # Add fire intensity info
+  
+  # Create a network based on HydroRIVERS
+  net <- as_sfnetwork(hyriv_line, directed = T)
+  # Update river network with outlets
+  new_net <- net %>% # Original river network
+    activate("nodes") %>%
+    st_network_blend(dir_outlets, tolerance = Inf) %>% # Blend in catchment outlets
+    convert(to_spatial_subdivision) %>%
+    activate("edges") %>%
+    mutate(weight = re_th_s26) # Use fire intensity as the weight for each river reach
+  # Create origin-destination (OD) matrix (distance-based, here)
+  POI_outlets <- st_as_sf(new_net, "nodes") %>%
+    filter(is.na(name) == F) # Recreate the outlet layer based on their new position after blending (POIs)
+  od_mat <- st_network_cost(new_net, 
+                            from = POI_outlets, to = POI_outlets, # from-to point sf
+                            direction = "in", # inbound
+                            Inf_as_NaN = T)
+  
+  od_mat_tibble <- tibble::as_tibble(od_mat, rownames = "id", .name_repair = "minimal") # converts matrix to tibble and add HYBAS_ID as column
+  od_mat_tibble <- rename(od_mat_tibble, HYBAS_ID = id) # Rename id field to HYBAS_ID for join
+  
+  # Total downstream fire intensity accumulation
+  # Row-wise aggregation (upstream looking downstream, i.e., accumulation)
+  routed_RE26 <- od_mat_tibble %>%
+    mutate(re_th_u26 = rowSums(pick(where(is.numeric), -HYBAS_ID), na.rm = T)) %>%
+    mutate(HYBAS_ID = as.numeric(HYBAS_ID)) %>%
+    select(HYBAS_ID, re_th_u26)
+  
+  ###########################
+  # Erosivity RCP45 routing #
+  ###########################
+  
+  hybas_RE45 <- hybas %>%
+    select(re_th_s45)
+  
+  # Reformat HydroRIVERS
+  hyriv_line <- st_cast(hyriv, to = "LINESTRING") %>%
+    st_join(hybas_RE45, join = st_intersects) # Add fire intensity info
+  
+  # Create a network based on HydroRIVERS
+  net <- as_sfnetwork(hyriv_line, directed = T)
+  # Update river network with outlets
+  new_net <- net %>% # Original river network
+    activate("nodes") %>%
+    st_network_blend(dir_outlets, tolerance = Inf) %>% # Blend in catchment outlets
+    convert(to_spatial_subdivision) %>%
+    activate("edges") %>%
+    mutate(weight = re_th_s45) # Use fire intensity as the weight for each river reach
+  # Create origin-destination (OD) matrix (distance-based, here)
+  POI_outlets <- st_as_sf(new_net, "nodes") %>%
+    filter(is.na(name) == F) # Recreate the outlet layer based on their new position after blending (POIs)
+  od_mat <- st_network_cost(new_net, 
+                            from = POI_outlets, to = POI_outlets, # from-to point sf
+                            direction = "in", # inbound
+                            Inf_as_NaN = T)
+  
+  od_mat_tibble <- tibble::as_tibble(od_mat, rownames = "id", .name_repair = "minimal") # converts matrix to tibble and add HYBAS_ID as column
+  od_mat_tibble <- rename(od_mat_tibble, HYBAS_ID = id) # Rename id field to HYBAS_ID for join
+  
+  # Total downstream fire intensity accumulation
+  # Row-wise aggregation (upstream looking downstream, i.e., accumulation)
+  routed_RE45 <- od_mat_tibble %>%
+    mutate(re_th_u45 = rowSums(pick(where(is.numeric), -HYBAS_ID), na.rm = T)) %>%
+    mutate(HYBAS_ID = as.numeric(HYBAS_ID)) %>%
+    select(HYBAS_ID, re_th_u45)
+  
+  ###########################
+  # Erosivity RCP85 routing #
+  ###########################
+  
+  hybas_RE85 <- hybas %>%
+    select(re_th_s85)
+  
+  # Reformat HydroRIVERS
+  hyriv_line <- st_cast(hyriv, to = "LINESTRING") %>%
+    st_join(hybas_RE85, join = st_intersects) # Add fire intensity info
+  
+  # Create a network based on HydroRIVERS
+  net <- as_sfnetwork(hyriv_line, directed = T)
+  # Update river network with outlets
+  new_net <- net %>% # Original river network
+    activate("nodes") %>%
+    st_network_blend(dir_outlets, tolerance = Inf) %>% # Blend in catchment outlets
+    convert(to_spatial_subdivision) %>%
+    activate("edges") %>%
+    mutate(weight = re_th_s85) # Use fire intensity as the weight for each river reach
+  # Create origin-destination (OD) matrix (distance-based, here)
+  POI_outlets <- st_as_sf(new_net, "nodes") %>%
+    filter(is.na(name) == F) # Recreate the outlet layer based on their new position after blending (POIs)
+  od_mat <- st_network_cost(new_net, 
+                            from = POI_outlets, to = POI_outlets, # from-to point sf
+                            direction = "in", # inbound
+                            Inf_as_NaN = T)
+  
+  od_mat_tibble <- tibble::as_tibble(od_mat, rownames = "id", .name_repair = "minimal") # converts matrix to tibble and add HYBAS_ID as column
+  od_mat_tibble <- rename(od_mat_tibble, HYBAS_ID = id) # Rename id field to HYBAS_ID for join
+  
+  # Total downstream fire intensity accumulation
+  # Row-wise aggregation (upstream looking downstream, i.e., accumulation)
+  routed_RE85 <- od_mat_tibble %>%
+    mutate(re_th_u85 = rowSums(pick(where(is.numeric), -HYBAS_ID), na.rm = T)) %>%
+    mutate(HYBAS_ID = as.numeric(HYBAS_ID)) %>%
+    select(HYBAS_ID, re_th_u85)
+  
+  
+  ## Create spatial layers -----------------------------------------------------
+  
+  hybas_routed_allvar <- inner_join(hybas, routed_FI) %>%
+    inner_join(., routed_BP) %>%
+    inner_join(., routed_RE) %>%
+    inner_join(., routed_RE26) %>%
+    inner_join(., routed_RE45) %>%
+    inner_join(., routed_RE85)
+  
+  ## Save outputs --------------------------------------------------------------
+  st_write(hybas_routed_allvar, "huc12_Valid_routed.gpkg", delete_layer = T)
   
   ## Clear memory
   gc(verbose = T, reset = T, full = T)
